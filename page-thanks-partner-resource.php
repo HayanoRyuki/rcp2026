885a19755606d22a95a03785638ee684dc9f4574
HayanoRyuki <hayano@media-confidence.com>
2025-12-03 20:57:25 +0900

    update: ○○を調整

--- page-thanks-partner-resource.php
+++ page-thanks-partner-resource.php
@@ -6,10 +6,10 @@ get_header('partner');
 <main class="thanks-page partner-thanks">
   <div class="container_l">
 
-    <h1 class="thanks-title">資料をダウンロードいただき、ありがとうございます。</h1>
+    <h1 class="thanks-title">資料をダウンロードいただき、<br>ありがとうございます。</h1>
 
     <p class="thanks-lead">
-      資料請求ありがとうございます。入力いただいたメールアドレス宛に資料をお送りしておりますので、ご確認お願いいたします。
+      資料請求ありがとうございます。入力いただいたメールアドレス宛に<br>資料をお送りしておりますので、ご確認お願いいたします。
     </p>
 
   </div>
