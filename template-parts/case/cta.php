5ef977fa2fce2b77d607a6e2fff857d88cc65f44
HayanoRyuki <hayano@media-confidence.com>
2025-12-16 19:19:49 +0900

    add: case common CTA as template part

---
+++ template-parts/case/cta.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Case study common CTA
+ */
+?>
+
+<div class="case-study-cta">
+  <!-- PC用 -->
+  <a href="https://t.receptionist.jp/RECEPTIONIST-cases/" class="case-study-banner-pc">
+    <img
+      src="https://receptionist.jp/wp-content/uploads/2024/11/CaseStudy_RequestBanner_A_PC.png"
+      alt="導入事例集（PC）"
+    >
+  </a>
+
+  <!-- SP用 -->
+  <a href="https://t.receptionist.jp/RECEPTIONIST-cases/" class="case-study-banner-sp">
+    <img
+      src="https://receptionist.jp/wp-content/uploads/2024/11/CaseStudy_RequestBanner_A_SP.png"
+      alt="導入事例集（SP）"
+    >
+  </a>
+</div>
