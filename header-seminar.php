ef706aea310cd45ee0a2e4139200c8ea3b0059b5
HayanoRyuki <hayano@media-confidence.com>
2026-02-06 08:22:23 +0000

    update: セミナーページ改修（業者対応分）

--- header-seminar.php
+++ header-seminar.php
@@ -1,9 +1,12 @@
 <?php
+
 /**
  * Header for Seminar LP
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -13,32 +16,28 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
-<!-- header -->
-<header class="l-header">
-  <div class="l-header__inner">
-    <a href="<?php echo esc_url(home_url('/')); ?>" class="l-header__logo">
-      <img
-  class="l-header__logo-img"
-  src="<?php echo get_template_directory_uri(); ?>/assets/img/seminar/logo.svg"
-  alt="RECEPTIONIST"
-  width="173"
-  height="42"
->
-    </a>
+  <!-- header -->
+  <header class="lp-seminar__header">
+    <div class="lp-seminar____inner">
+      <a href="<?php echo esc_url(home_url('/')); ?>" class="lp-seminar__header-logo">
+        <img
+          class="lp-seminar__header-logo-img"
+          src="<?php echo get_template_directory_uri(); ?>/assets/img/seminar/logo.svg"
+          alt="RECEPTIONIST"
+          width="173"
+          height="42">
+      </a>
 
-    <!-- pc nav -->
-    <nav class="l-header__nav">
-      <p class="l-header__nav-text">
-        受付システムをもっと活用したい方へ！
-        <span>無料講習会</span>にご招待！
-      </p>
-      <div class="l-header__nav-cta">
-        <a class="c-button__header u-pc" href="#form">申し込む</a>
-        <a class="c-button__header u-sp" href="#form">申し込む</a>
-      </div>
-    </nav>
-  </div>
-</header>
-<!-- /header -->
+      <!-- pc nav -->
+      <nav class="lp-seminar__header-nav">
+        <p class="lp-seminar__header-nav-text">受付システムをもっと活用したい方へ！<span>無料講習会</span>にーご招待！</p>
+        <div class="lp-seminar__header-nav-cta">
+          <p class="lp-seminar__header-nav-label"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/seminar/icn_online.svg" alt="オンライン開催" width="12.81"
+              height="15.73">オンライン開催</p>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!-- /header -->
